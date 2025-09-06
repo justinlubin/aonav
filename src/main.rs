949a7cccf3b06bff80b7f40ee14f4b43ae6900e4
Marlena Preigh <mjpreigh@cs.washington.edu>
2025-09-05 17:24:52 -0700

    egraph to and/or rough

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use serde::ser::SerializeMap;
 use under::ao;
 use under::session;
+use under::serialize;
+use egg::*;
+use std::fmt::Display;
+use std::path;
 
 fn main() {
+    let mut eg: EGraph<SymbolLang, ()> = Default::default();
+    serialize::get_simple_egraph(&mut eg);
+    eg.dot().to_svg("foo.svg").unwrap();
+    let out = serialize::egg_to_serialized_egraph(&mut eg);
+    out.to_json_file("out.txt");
+    serialize::egg_to_and_or(&eg, String::from("test"));
+
     // session::Session::new().go()
-    ao::main()
+    //ao::main()
 }
---
+++ src/serialize.rs
@@ -0,0 +1,117 @@
+// serialize egraph to our and/or format
+
+// what are root e-classes and do they matter here
+
+use egg::*;
+use indexmap::IndexMap;
+use std::{fmt::Display, fs::File, io::prelude::*};
+use crate::jgf;
+use serde_json;
+
+pub fn get_simple_egraph(eg: &mut EGraph<egg::SymbolLang, ()>) {
+    //let mut eg: EGraph<SymbolLang, ()> = Default::default();
+    let a_class = eg.add(SymbolLang::leaf("a"));
+    let b_class = eg.add(SymbolLang::leaf("b"));
+    //let ab_class = eg.union(a_class, b_class);
+    eg.add(SymbolLang::new("foo", vec![a_class, b_class]));
+    let ab_class = eg.union(a_class, b_class);
+    eg.rebuild();
+}
+
+
+// copy-paste from egraph-serialize
+pub fn egg_to_serialized_egraph<L, A>(egraph: &EGraph<L, A>) -> egraph_serialize::EGraph
+where
+    L: Language + Display,
+    A: Analysis<L>,
+{
+    use egraph_serialize::*;
+    let mut out = EGraph::default();
+    for class in egraph.classes() {
+        for (i, node) in class.nodes.iter().enumerate() {
+            out.add_node(
+                format!("{}.{}", class.id, i),
+                Node {
+                    op: node.to_string(),
+                    children: node
+                        .children()
+                        .iter()
+                        .map(|id| NodeId::from(format!("{}.0", id)))
+                        .collect(),
+                    eclass: ClassId::from(format!("{}", class.id)),
+                    cost: Cost::new(1.0).unwrap(),
+                    subsumed: false,
+                },
+            )
+        }
+    }
+    out
+}
+
+
+// strongly inspired by egraph-serialize
+// serializes egraph into and/or format in ao-examples/name.json
+pub fn egg_to_and_or<L, A>(egraph: &EGraph<L, A>, name: String)
+where
+    L: Language + Display,
+    A: Analysis<L>,
+{
+
+    let mut edges = Vec::new();
+    let mut nodes: IndexMap<String, jgf::Node> = IndexMap::new();
+    for class in egraph.classes() {
+        // add AND node for class
+        let mut and_metadata = IndexMap::new();
+        and_metadata.insert(String::from("kind"), serde_json::Value::String(String::from("AND")));
+        nodes.insert(class.id.to_string(), jgf::Node {
+            label: Some(class.id.to_string()), 
+            metadata: Some(and_metadata),
+            });
+        for (i, node) in class.nodes.iter().enumerate() {
+            // add OR node for node
+            let mut or_metadata = IndexMap::new();
+            or_metadata.insert(String::from("kind"), serde_json::Value::String(String::from("OR")));
+            nodes.insert(node.to_string(), jgf::Node {
+                label: Some(node.to_string()), 
+                metadata: Some(or_metadata),
+            });
+            // add edge from class to node
+            edges.push(jgf::Edge{
+                    id: None,
+                    source: class.id.to_string(),
+                    target: node.to_string(),
+                    relation: None,
+                    directed: true,
+                    label: None,
+                    metadata: None
+                    }
+                );
+            // add edge from node to each child class
+            for child in node.children() {
+                edges.push(jgf::Edge{
+                    id: None,
+                    source: node.to_string(),
+                    target: child.to_string(),
+                    relation: None,
+                    directed: true,
+                    label: None,
+                    metadata: None
+                    }
+                );
+            }
+        }
+    }
+
+    let mut and_or = jgf::Graph{
+        id: Some(String::from(name)),
+        label: Some(String::from ("label?")),
+        directed: true,
+        graph_type: None,
+        metadata: None,
+        nodes: Some(nodes),
+        edges: Some(edges)
+    };
+    let to_json = serde_json::to_string_pretty(&and_or).expect("Failed to go from struct to pretty json");
+    let mut file = File::create("name.json").expect("Failed to create file");
+    file.write_all(to_json.as_bytes()).expect("Failed to write");
+}
