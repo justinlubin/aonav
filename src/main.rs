2a7e6370ad20354b4c8beb51b78682dcea48c317
Marlena Preigh <mjpreigh@cs.washington.edu>
2025-09-10 11:16:39 -0700

    changed function names in serialize

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-
-use serde::ser::SerializeMap;
-use under::session;
-use under::serialize;
 use egg::*;
+use serde::ser::SerializeMap;
 use std::fmt::Display;
 use std::path;
+use under::serialize;
+use under::session;
 
 use under::ao;
 
@@ -13,11 +12,11 @@ fn main() {
     let mut eg: EGraph<SymbolLang, ()> = Default::default();
     serialize::get_simple_egraph(&mut eg);
     eg.dot().to_svg("foo.svg").unwrap();
-    //let out = serialize::egg_to_serialized_egraph(&mut eg);
-    //out.to_json_file("out.txt");
-    serialize::egg_to_and_or(&eg, String::from("test"));
+    let out = serialize::egraph_to_serialized_egraph(&mut eg);
+    out.to_json_file("out.txt");
+    serialize::egraph_to_and_or(&eg, String::from("test"));
     */
-    
+
     //session::Session::new().go();
     ao::main()
 }
