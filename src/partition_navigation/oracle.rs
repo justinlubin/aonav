c5e299091402d2bb62666e9b4f0923650ef73664
Justin Lubin <jlubi333@gmail.com>
2025-11-04 14:54:18 -0800

    Overhal oracle, work correctly now (cannot do optimistic assumption due to F nodes)

--- src/partition_navigation/oracle.rs
+++ src/partition_navigation/oracle.rs
@@ -1,10 +1,13 @@
 use crate::ao;
 use crate::partition_navigation::*;
+use crate::pbn;
 
 use rustsat::instances::SatInstance;
 use rustsat::solvers::Solve;
 use rustsat::types::{constraints::CardConstraint, Lit};
 use std::collections::HashMap;
+
+// Helpers
 
 fn add_lit_eq_cube(instance: &mut SatInstance, a: Lit, b: &[Lit]) {
     instance.add_lit_impl_cube(a, b);
@@ -16,197 +19,244 @@ fn add_lit_eq_clause(instance: &mut SatInstance, a: Lit, b: &[Lit]) {
     instance.add_clause_impl_lit(b, a);
 }
 
-struct Vars {
+fn add_lit_eq_lit(instance: &mut SatInstance, a: Lit, b: Lit) {
+    instance.add_lit_impl_lit(a, b);
+    instance.add_lit_impl_lit(b, a);
+}
+
+fn add_guarded_lit(instance: &mut SatInstance, guard: Lit, a: Lit, b: Lit) {
+    instance.add_cube_impl_lit(&[guard, a], b);
+    instance.add_cube_impl_lit(&[guard, b], a);
+}
+
+fn add_guarded_lit_eq_lit(
+    instance: &mut SatInstance,
+    guard: Lit,
+    a: Lit,
+    b: Lit,
+) {
+    instance.add_cube_impl_lit(&[guard, a], b);
+    instance.add_cube_impl_lit(&[guard, b], a);
+}
+
+fn add_guarded_lit_eq_clause(
+    instance: &mut SatInstance,
+    guard: Lit,
+    a: Lit,
+    b: &[Lit],
+) {
+    instance.add_cube_impl_clause(&[guard, a], b);
+    for bi in b {
+        instance.add_cube_impl_lit(&[guard, *bi], a);
+    }
+}
+
+fn add_guarded_unit(instance: &mut SatInstance, guard: Lit, unit: Lit) {
+    instance.add_lit_impl_lit(guard, unit);
+}
+
+// Compilation
+
+struct Context<'a> {
+    instance: SatInstance,
+    exp: &'a Exp,
+    // Whether or not OR node should use assume semantics
+    o_assume: HashMap<ao::OIdx, Lit>,
+    // OR node truth values
     o_true: HashMap<ao::OIdx, Lit>,
-    a_true: HashMap<ao::AIdx, Lit>,
+    // OR node on chosen derivation tree
     o_active: HashMap<ao::OIdx, Lit>,
+    // AND node truth values
+    a_true: HashMap<ao::AIdx, Lit>,
+    // AND node on chosen derivation tree
     a_active: HashMap<ao::AIdx, Lit>,
 }
 
-fn make_vars(instance: &mut SatInstance, e: &Exp) -> Vars {
-    // OR node truth values
-    let mut o_true = HashMap::new();
-
-    // AND node truth values
-    let mut a_true = HashMap::new();
-
-    // OR node on chosen derivation tree
-    let mut o_active = HashMap::new();
-
-    // AND node on chosen derivation tree
-    let mut a_active = HashMap::new();
-
-    for oidx in e.graph().or_indexes() {
-        o_true.insert(oidx, instance.new_lit());
-        o_active.insert(oidx, instance.new_lit());
-    }
-
-    for aidx in e.graph().and_indexes() {
-        a_true.insert(aidx, instance.new_lit());
-        a_active.insert(aidx, instance.new_lit());
-    }
-
-    Vars {
-        o_true,
-        a_true,
-        o_active,
-        a_active,
-    }
-}
-
-fn compile(instance: &mut SatInstance, vars: &Vars, e: &Exp) {
-    let graph = e.graph();
-    let Vars {
-        o_true,
-        a_true,
-        o_active,
-        a_active,
-    } = vars;
-
-    // Add OR node constraints (semantic, activity, and consistency)
-
-    for oidx in graph.or_indexes() {
-        let is_true = *o_true.get(&oidx).unwrap();
-        let is_active = *o_active.get(&oidx).unwrap();
-        let class = e.class(oidx);
-
-        // Add semantic and activity constraints
-
-        // --- Activity (universal) ---
-        instance.add_lit_impl_lit(is_active, is_true);
-
-        // If non-goal OR node active, then at least one consumer is active
-        if oidx != graph.goal() {
-            let consumers_active = graph
-                .consumers(oidx)
-                .map(|a| *a_active.get(&a).unwrap())
-                .collect::<Vec<_>>();
-
-            instance.add_lit_impl_clause(is_active, &consumers_active[..]);
-        }
-
-        // Optimistically assume all unseen and T/A nodes are assumed
-        let assume_semantics = match class {
-            Class::True {
-                assume: None | Some(true),
-                ..
-            }
-            | Class::Unseen => true,
-            _ => false,
+impl<'a> Context<'a> {
+    fn compile(instance: SatInstance, exp: &'a Exp) -> Self {
+        let mut ret = Self {
+            instance,
+            exp,
+            o_assume: HashMap::new(),
+            o_true: HashMap::new(),
+            o_active: HashMap::new(),
+            a_true: HashMap::new(),
+            a_active: HashMap::new(),
         };
 
-        if assume_semantics {
-            // --- Semantic ---
-
-            // Assume true
-            instance.add_unit(is_true);
-
-            // --- Activity ---
-
-            // Providers cannot be active
-            for aidx in graph.providers(oidx) {
-                instance.add_unit(!*a_active.get(&aidx).unwrap());
-            }
-        } else {
-            // --- Semantic ---
-
-            // OR node true iff at least one provider true
-            add_lit_eq_clause(
-                instance,
-                is_true,
-                &graph
-                    .providers(oidx)
-                    .map(|a| *a_true.get(&a).unwrap())
-                    .collect::<Vec<_>>()[..],
-            );
-
-            // --- Activity ---
-
-            let providers_active = graph
-                .providers(oidx)
-                .map(|a| *a_active.get(&a).unwrap())
-                .collect::<Vec<_>>();
-
-            // If at least one provider is active, then OR node is active
-            instance.add_clause_impl_lit(&providers_active[..], is_active);
-
-            // If OR node is active, then at least one provider is active
-            instance.add_lit_impl_clause(is_active, &providers_active[..]);
-
-            // At most one provider is active
-            instance.add_card_constr(CardConstraint::new_ub(
-                providers_active.into_iter(),
-                1,
-            ));
+        for oidx in exp.graph().or_indexes() {
+            ret.o_assume.insert(oidx, ret.instance.new_lit());
+            ret.o_true.insert(oidx, ret.instance.new_lit());
+            ret.o_active.insert(oidx, ret.instance.new_lit());
         }
 
-        // Add consistency constraints
+        for aidx in exp.graph().and_indexes() {
+            ret.a_true.insert(aidx, ret.instance.new_lit());
+            ret.a_active.insert(aidx, ret.instance.new_lit());
+        }
 
-        match class {
-            Class::Unseen => (),
-            Class::Unknown => (),
-            Class::False => instance.add_unit(!is_true),
-            Class::True {
-                force_use,
-                assume: Some(false),
-            } => {
-                instance.add_unit(is_true);
-                if force_use {
-                    instance.add_unit(is_active);
-                }
-            }
-            Class::True {
-                force_use,
-                assume: _,
-            } => {
-                if force_use {
-                    instance.add_unit(is_active);
-                }
-            }
+        ret.add_constraints();
+
+        ret
+    }
+
+    fn add_constraints(&mut self) {
+        for oidx in self.exp.graph().or_indexes() {
+            self.or(oidx)
+        }
+
+        for aidx in self.exp.graph().and_indexes() {
+            self.and(aidx)
         }
     }
 
-    // Add AND node constraints (semantic, activity)
+    fn or_semantics(&mut self, oidx: ao::OIdx) {
+        let graph = self.exp.graph();
 
-    for aidx in graph.and_indexes() {
-        let is_true = *a_true.get(&aidx).unwrap();
-        let is_active = *a_active.get(&aidx).unwrap();
+        let is_assume = *self.o_assume.get(&oidx).unwrap();
+        let is_true = *self.o_true.get(&oidx).unwrap();
 
-        // --- Semantic ---
-
-        // AND node true iff premises true
-        add_lit_eq_cube(
-            instance,
+        // Non-assume OR node true iff a provider is true
+        add_guarded_lit_eq_clause(
+            &mut self.instance,
+            !is_assume,
             is_true,
             &graph
-                .premises(aidx)
-                .map(|o| *o_true.get(&o).unwrap())
+                .providers(oidx)
+                .map(|a| *self.a_true.get(&a).unwrap())
                 .collect::<Vec<_>>()[..],
         );
 
-        // --- Activity ---
+        // Assume OR node unconditionally true
+        self.instance.add_lit_impl_lit(is_assume, is_true);
+    }
+
+    fn or_activity(&mut self, oidx: ao::OIdx) {
+        let graph = self.exp.graph();
+
+        let is_assume = *self.o_assume.get(&oidx).unwrap();
+        let is_true = *self.o_true.get(&oidx).unwrap();
+        let is_active = *self.o_active.get(&oidx).unwrap();
 
         // Active implies true
-        instance.add_lit_impl_lit(is_active, is_true);
+        self.instance.add_lit_impl_lit(is_active, is_true);
+
+        if oidx != graph.goal() {
+            let consumers_active = graph
+                .consumers(oidx)
+                .map(|a| *self.a_active.get(&a).unwrap())
+                .collect::<Vec<_>>();
+
+            // Active non-goal nodes must have active consumer
+            self.instance
+                .add_lit_impl_clause(is_active, &consumers_active[..]);
+        }
+
+        let providers_active = graph
+            .providers(oidx)
+            .map(|a| *self.a_active.get(&a).unwrap())
+            .collect::<Vec<_>>();
+
+        // Providers of assume nodes cannot be active
+        self.instance.add_lit_impl_cube(
+            is_assume,
+            &providers_active.iter().map(|x| !*x).collect::<Vec<_>>()[..],
+        );
+
+        // Non-assume OR node active iff at least one provider active
+        add_guarded_lit_eq_clause(
+            &mut self.instance,
+            !is_assume,
+            is_active,
+            &providers_active[..],
+        );
+
+        // Unconditionally true that at most one provider is active
+        self.instance.add_card_constr(CardConstraint::new_ub(
+            providers_active.into_iter(),
+            1,
+        ));
+    }
+
+    fn or_consistency(&mut self, oidx: ao::OIdx) {
+        let class = self.exp.class(oidx);
+
+        let is_assume = *self.o_assume.get(&oidx).unwrap();
+        let is_true = *self.o_true.get(&oidx).unwrap();
+        let is_active = *self.o_active.get(&oidx).unwrap();
+
+        match class {
+            Class::Unseen => (),
+            Class::Unknown => self.instance.add_unit(!is_assume),
+            Class::False => {
+                self.instance.add_unit(!is_assume);
+                self.instance.add_unit(!is_true);
+                self.instance.add_unit(!is_active);
+            }
+            Class::True { force_use, assume } => {
+                self.instance.add_unit(is_true);
+                if force_use {
+                    self.instance.add_unit(is_active);
+                }
+                match assume {
+                    Some(false) => self.instance.add_unit(!is_assume),
+                    Some(true) => self.instance.add_unit(is_assume),
+                    None => (),
+                }
+            }
+        }
+    }
+
+    fn or(&mut self, oidx: ao::OIdx) {
+        self.or_semantics(oidx);
+        self.or_activity(oidx);
+        self.or_consistency(oidx);
+    }
+
+    fn and_semantics(&mut self, aidx: ao::AIdx) {
+        let is_true = *self.a_true.get(&aidx).unwrap();
+
+        add_lit_eq_cube(
+            &mut self.instance,
+            is_true,
+            &self
+                .exp
+                .graph()
+                .premises(aidx)
+                .map(|o| *self.o_true.get(&o).unwrap())
+                .collect::<Vec<_>>()[..],
+        );
+    }
+
+    fn and_activity(&mut self, aidx: ao::AIdx) {
+        let graph = self.exp.graph();
+
+        let is_true = *self.a_true.get(&aidx).unwrap();
+        let is_active = *self.a_active.get(&aidx).unwrap();
+
+        // Active implies true
+        self.instance.add_lit_impl_lit(is_active, is_true);
 
         let premises_active = graph
             .premises(aidx)
-            .map(|o| *o_active.get(&o).unwrap())
+            .map(|o| *self.o_active.get(&o).unwrap())
             .collect::<Vec<_>>();
 
-        // if AND node is active, then all premises active
-        instance.add_lit_impl_cube(is_active, &premises_active[..]);
+        // AND node is active implies all premises active
+        self.instance
+            .add_lit_impl_cube(is_active, &premises_active[..]);
+    }
+
+    fn and(&mut self, aidx: ao::AIdx) {
+        self.and_semantics(aidx);
+        self.and_activity(aidx);
     }
 }
 
 #[allow(dead_code)]
 pub fn nonempty_completion(e: &Exp) -> bool {
-    let mut instance = SatInstance::new();
-
-    let vars = make_vars(&mut instance, e);
-
-    compile(&mut instance, &vars, e);
-    let cnf = instance.into_cnf().0;
+    let ctx = Context::compile(SatInstance::new(), e);
+    let cnf = ctx.instance.into_cnf().0;
 
     let mut solver = rustsat_batsat::BasicSolver::default();
     solver.add_cnf(cnf).unwrap();
@@ -216,19 +266,44 @@ pub fn nonempty_completion(e: &Exp) -> bool {
     if ok && log::log_enabled!(log::Level::Debug) {
         log::debug!("{}", e);
         let sol = solver.full_solution().unwrap();
-        for (oidx, lit) in vars.o_true {
+        for (oidx, lit) in ctx.o_true {
             log::debug!("{} true: {}", e.graph().or_at(oidx), sol[lit.var()])
         }
-        for (oidx, lit) in vars.o_active {
+        for (oidx, lit) in ctx.o_active {
             log::debug!("{} active: {}", e.graph().or_at(oidx), sol[lit.var()])
         }
-        for (aidx, lit) in vars.a_true {
+        for (aidx, lit) in ctx.a_true {
             log::debug!("{} true: {}", e.graph().and_at(aidx), sol[lit.var()])
         }
-        for (aidx, lit) in vars.a_active {
+        for (aidx, lit) in ctx.a_active {
             log::debug!("{} active: {}", e.graph().and_at(aidx), sol[lit.var()])
         }
     }
 
     ok
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Validity checker
+
+pub fn valid(e: &Exp) -> bool {
+    let mut e = e.clone();
+    e.set_remaining_unknown();
+    nonempty_completion(&e)
+}
+
+pub struct Valid;
+
+impl Valid {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl pbn::ValidityChecker for Valid {
+    type Exp = Exp;
+
+    fn check(&self, e: &Self::Exp) -> bool {
+        valid(e)
+    }
 }
