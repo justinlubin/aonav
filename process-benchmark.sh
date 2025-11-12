c7f8eec7c1d10c76c70e259eb5152e8e18a5d364
Justin Lubin <jlubi333@gmail.com>
2025-11-12 15:09:02 -0800

    Reduced argus

--- process-benchmark.sh
+++ process-benchmark.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p benchmark
-rm -rf benchmark
-mkdir -p benchmark
+# mkdir -p benchmark
+# rm -rf benchmark
+# mkdir -p benchmark
 
 mkdir -p benchmark-reduced
 rm -rf benchmark-reduced
@@ -11,10 +11,14 @@ mkdir -p benchmark-reduced
 cd raw-benchmark
 cargo b
 for f in *.json; do
-    ../target/debug/under convert -f AOJsonGraph --randomize "$f" > "../benchmark/$f"
-    # ../target/debug/under convert -f AOJsonGraph --reduce --randomize "$f" > "../benchmark-reduced/$f"
+    # ../target/debug/under convert -f AOJsonGraph --randomize "$f" > "../benchmark/$f"
+    case "$f" in argus*)
+        echo $f
+        ../target/debug/under convert -f AOJsonGraph --reduce --randomize "$f" > "../benchmark-reduced/$f"
+    esac
 done
 
 cd ..
 
-./target/debug/under generate-solutions -c 1 --parallel benchmark
+# ./target/debug/under generate-solutions -c 1 --parallel benchmark
+./target/debug/under generate-solutions -c 1 --parallel benchmark-reduced
