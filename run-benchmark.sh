c60a449c361328951c43b84490224fa12bf441b3
Justin Lubin <jlubi333@gmail.com>
2025-11-12 14:10:59 -0800

    Run some benchmarks

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -2,4 +2,4 @@
 
 cargo b
 # ./target/debug/under benchmark -p Re,AU,AC -r 1 --parallel benchmark > results/prelim.csv
-./target/debug/under benchmark -p AU,AC,AR -r 1 --parallel benchmark > results/prelim-relevant.csv
+./target/debug/under benchmark -p AU,AC,AR -r 3 --parallel --minimal benchmark > results/minimal.csv
