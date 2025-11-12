d06c27d8dbb48a6e007fb01c838efed0b557ed07
Justin Lubin <jlubi333@gmail.com>
2025-11-12 11:38:57 -0800

    Add processed benchmarks

---
+++ process-benchmark.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+mkdir -p benchmark
+rm -rf benchmark
+mkdir -p benchmark
+
+mkdir -p benchmark-reduced
+rm -rf benchmark-reduced
+mkdir -p benchmark-reduced
+
+cd raw-benchmark
+cargo b
+for f in *.json; do
+    ../target/debug/under convert -f AOJsonGraph --randomize "$f" > "../benchmark/$f"
+    ../target/debug/under convert -f AOJsonGraph --reduce --randomize "$f" > "../benchmark-reduced/$f"
+done
