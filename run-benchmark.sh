ce8c4a2c2c9b0a4426813cf0c89820370c277bb5
Justin Lubin <jlubi333@gmail.com>
2025-11-12 13:04:14 -0800

    Update run-benchmark for minimal too

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 cargo b
-./target/debug/under benchmark -p Re,AU,AC --parallel benchmark > results/prelim.csv
+# ./target/debug/under benchmark -p Re,AU,AC -r 1 --parallel benchmark > results/prelim.csv
+./target/debug/under benchmark -p AU,AC,AR -r 1 --parallel benchmark > results/prelim-relevant.csv
