a321e3a82f46176b5542d9d8e27d8589eed534f8
Justin Lubin <jlubi333@gmail.com>
2026-02-06 13:45:40 -0800

    Organize benchmarks

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-cargo b
-# ./target/debug/under benchmark -p Re,AU,AC -r 1 --parallel benchmark > results/prelim.csv
-# ./target/debug/under benchmark -p AU,AC,AR -r 3 --parallel --minimal benchmark > results/minimal.csv
-./target/debug/under benchmark -p AU,AC,AR -r 3 --parallel --minimal benchmark > results/benchmark-cadical.csv
-./target/debug/under benchmark -p AU,AC,AR -r 3 --parallel --minimal benchmark-reduced > results/benchmark-reduced-cadical.csv
-# ./target/debug/under benchmark -p MIGR -r 3 --parallel --minimal benchmark-reduced > results/benchmark-reduced-migr.csv
+cargo build
+
+for suite in $(ls benchmark/suites); do
+  ./target/debug/under benchmark \
+    --parallel \
+    --replicates 3 \
+    --minimal \
+    --providers AlphabeticalUnsound,AlphabeticalComplete,AlphabeticalRelevant \
+    "benchmark/suites/${suite}" \
+    > "benchmark/results/${suite}.csv"
+done
