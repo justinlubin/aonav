be5bf3108de716fefc88734b2cdd006051f8fc3a
Justin Lubin <jlubi333@gmail.com>
2026-02-09 13:52:39 -0800

    Add aesop benchmark

---
+++ scripts/deduplicate-aesop.py
@@ -0,0 +1,16 @@
+import glob
+import sys
+import os
+
+parent_dir = sys.argv[1]
+
+contents = set()
+
+for path in glob.glob(f"{parent_dir}/input/*.json"):
+    with open(path, "r") as f:
+        content = f.read()
+        if content in contents:
+            continue
+        contents.add(content)
+        with open(f"{parent_dir}/output/aesop-{os.path.basename(path)}", "w") as out:
+            out.write(content)
