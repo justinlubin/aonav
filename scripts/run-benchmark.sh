7552bb01884ff1f1b6fb6507ac234fcd62dc8f1c
Justin Lubin <jlubi333@gmail.com>
2026-02-13 16:52:53 -0800

    Add timeout to benchmark script

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -6,23 +6,26 @@ cargo build
 
 run() {
   echo "Running provider '$2' on suite '$1'..."
+  
   ./target/debug/under benchmark \
     --parallel \
     --replicates 3 \
     --minimal \
+    --timeout 120 \
     --providers "$2" \
     "benchmark/suites/$1" \
     > "benchmark/results/$1-$2.csv"
+
   echo "Done!"
 }
 
-run "unreduced" "AlphabeticalUnsound"
-run "unreduced" "AlphabeticalComplete"
-run "unreduced" "AlphabeticalRelevant"
+for path in benchmark/suites/*; do
+  suite=$(basename $path)
 
-run "reduced" "AlphabeticalUnsound"
-run "reduced" "AlphabeticalComplete"
-run "reduced" "AlphabeticalRelevant"
+  run "$suite" "AlphabeticalUnsound"
+  run "$suite" "AlphabeticalComplete"
+  run "$suite" "AlphabeticalRelevant"
 
-run "nonrandom" "MaxInfoGain"
-run "nonrandom" "MaxInfoGainRelevant"
+  run "$suite" "MaxInfoGainRelevant"
+  run "$suite" "MaxInfoGain"
+done
