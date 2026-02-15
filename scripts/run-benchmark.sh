6d720f376aaea24726544d359b5c877f06df987f
Justin Lubin <jlubi333@gmail.com>
2026-02-14 16:53:38 -0800

    Finish organizing/generating benchmark entries

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -5,27 +5,27 @@ echo "Running on $RAYON_NUM_THREADS cores"
 cargo build
 
 run() {
-  echo "Running provider '$2' on suite '$1'..."
+  echo "Running provider '$2' on entry collection '$1'..."
   
   ./target/debug/under benchmark \
     --parallel \
     --replicates 3 \
     --minimal \
-    --timeout 120 \
+    --timeout 30 \
     --providers "$2" \
-    "benchmark/suites/$1" \
+    "benchmark/entries/$1" \
     > "benchmark/results/$1-$2.csv"
 
   echo "Done!"
 }
 
-for path in benchmark/suites/*; do
-  suite=$(basename $path)
+for path in benchmark/entries/*; do
+  collection=$(basename $path)
 
-  run "$suite" "AlphabeticalUnsound"
-  run "$suite" "AlphabeticalComplete"
-  run "$suite" "AlphabeticalRelevant"
+  run "$collection" "AlphabeticalUnsound"
+  run "$collection" "AlphabeticalComplete"
+  run "$collection" "AlphabeticalRelevant"
 
-  run "$suite" "MaxInfoGainRelevant"
-  run "$suite" "MaxInfoGain"
+  run "$collection" "MaxInfoGainRelevant"
+  run "$collection" "MaxInfoGain"
 done
