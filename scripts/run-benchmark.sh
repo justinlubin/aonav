879f285cbdfb64c457c54affd0fa270488aa5761
Justin Lubin <jlubi333@gmail.com>
2026-03-09 09:28:54 -0700

    Update benchmarking script

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -2,19 +2,29 @@
 
 echo "Running on $RAYON_NUM_THREADS cores"
 
-cargo build
+cargo build --release
 
 run() {
   echo "Running provider '$2' on entry collection '$1'..."
   
-  ./target/debug/under benchmark \
+  ./target/release/aonav benchmark \
     --parallel \
     --replicates 3 \
     --minimal \
     --timeout 30 \
     --providers "$2" \
     "benchmark/entries/$1" \
-    > "benchmark/results/$1-$2.csv"
+    > "benchmark/results/NON_INCR-$1-$2.csv"
+
+  ./target/release/aonav benchmark \
+    --parallel \
+    --replicates 3 \
+    --minimal \
+    --timeout 30 \
+    --providers "$2" \
+    --incremental \
+    "benchmark/entries/$1" \
+    > "benchmark/results/INCR-$1-$2.csv"
 
   echo "Done!"
 }
